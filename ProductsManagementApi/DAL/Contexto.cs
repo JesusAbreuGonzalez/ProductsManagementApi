be3f99ff50d951ee963798a7f174540642b4beb8
JesusAbreuGonzalez <77336425+JesusAbreuGonzalez@users.noreply.github.com>
2021-10-23 21:29:54 -0400

    Creando contexto y ConStr

---
+++ ProductsManagementApi/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductsManagementApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsManagementApi.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Productos> Productos { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+    }
+}
